97197e9ba197eeae68761693f88c3544bfb5db90
Marcelo Alckmin <marcelo.alckmin22@gmail.com>
2026-02-12 20:07:25 -0300

    feature: spring initializr base backend structure

---
+++ backend/api/src/test/java/com/timelinelp/api/TimelineLpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timelinelp.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimelineLpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
