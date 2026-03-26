cc67d1077d587a7787f4f80b29705ed19cb037d5
marceloalckmin <marcelo.alckmin22@gmail.com>
2026-03-26 19:59:26 -0300

    feat: timeline controller w/registering and listing user timelines by email done!

---
+++ backend/api/src/main/java/com/timelinelp/api/timeline/TimelineController.java
@@ -0,0 +1,31 @@
+package com.timelinelp.api.timeline;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.timelinelp.api.timeline.dto.*;
+
+@RestController
+@RequestMapping("/timeline")
+public class TimelineController {
+    private TimelineService service;
+
+    public TimelineController(TimelineService service){
+        this.service = service;
+    }
+
+    @PostMapping
+    public TimelineResponse registerTimeline(@RequestBody TimelineRequest timelineDto) throws Exception{
+        TimelineResponse response = service.registerTimeline(timelineDto);
+        return response;
+    }
+
+    public List<TimelineResponse> getUserTimelinesByEmail(@RequestBody String userEmail) throws Exception{
+        List<TimelineResponse> userTimelines = service.getTimelinesFromUser(userEmail);
+        return userTimelines;
+    }
+}
