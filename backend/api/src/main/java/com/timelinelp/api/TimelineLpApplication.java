32e80258eaf3dabca3ed92dc1e4b09ea09959380
marceloalckmin <marcelo.alckmin22@gmail.com>
2026-04-17 19:28:03 -0300

    teste ssh

--- backend/api/src/main/java/com/timelinelp/api/TimelineLpApplication.java
+++ backend/api/src/main/java/com/timelinelp/api/TimelineLpApplication.java
@@ -3,6 +3,8 @@ package com.timelinelp.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+//Teste do ambiente novo, depois tiro o comentario inutil
 @SpringBootApplication
 public class TimelineLpApplication {
 
